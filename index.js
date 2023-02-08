e86b6d0e3766e03e9b2c5882935db13118587899
Mohit Patel <mohit.patel1966@gmail.com>
2023-02-08 21:20:12 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -18,6 +19,45 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const links = []
+let id = 0
+app.use(bodyParser.urlencoded({extended: false}))
+app.post('app/shorturl',(req,res) => {
+  const { url } = req.body;
+  console.log("hello")
+  console.log(url)
+
+  dns.lookup(url, (err) => {
+    if(err) {
+      return res.json({
+        error: "invalid URL"
+      })
+    }else{
+      id++
+      let link = {
+        original_url: url,
+        short_url: `${id}`
+      }
+      links.push(link)
+      return res.json(link)
+    }
+  })
+})
+
+app.get('app/shorturl/:id', (req, res) => {
+  const { id } = req.params
+  const shortURL = links.find(l => l.short_url === id)
+
+  if (shortURL) {
+    return res.redirect(shortURL.original_url)
+  }else{
+    res.json({
+      error: "No short url"
+    })
+  }
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
